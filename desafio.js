09dde84f05bda1a84eedd55e06917aa990a62e3d
hrvieira <luizhrvieira@outlook.com>
2023-04-29 22:10:15 -0300

    Atualizações

--- desafio.js
+++ desafio.js
@@ -4,11 +4,11 @@ for (let indice = 0; indice < listaDeTeclas.length; indice++) {
   const tecla = listaDeTeclas[indice];
 
   tecla.onkeydown = function (evento) {
-    if(evento.code === "Enter" || evento.code === "Space") {
+    if(evento.code === 'Enter' || evento.code === 'Space') {
     tecla.classList.add('ativa');
     }
   }
-  
+
   tecla.onkeyup = function () {
     tecla.classList.remove('ativa');
   }
